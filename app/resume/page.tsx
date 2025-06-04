7b517423d4a865fe4b3fa737b79d5c52034ac7f4
nahel <nahelawad123@gmail.com>
2025-06-05 00:15:12 +0100

    updates

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -8,25 +8,21 @@ import Header from "@/components/header";
 export default function About() {
   return (
     <>
-      <div className="min-h-screen flex flex-col px-4 sm:px-8">
-        <div className="fixed top-0 left-0 w-full z-50">
-        <Header />
-      </div>
+  <div className="min-h-screen flex flex-col px-4 sm:px-8 pt-20">
+  <div className="fixed top-0 left-0 w-full z-50">
+    <Header />
+  </div>
 
+  <div className="flex justify-center items-center flex-grow">
+    <button 
+      onClick={() => console.log("Hi")} 
+      className="text-3xl font-press-start text-center hover:cursor-pointer "
+    >
+      Download My Cv
+    </button>
+  </div>
+</div>
 
-        <div className="h-45" />
-
-        <div className="content-center">
-            <h2 className="text-3xl font-press-start content-center text-center mt-50">
-
-                DOWNLOAD MY CV
-
-            </h2>
-
-        </div>
-
-       
-      </div>
     </>
   );
 }
